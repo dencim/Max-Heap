bec661d243d4e6c122fcfde99617a981c24ef351
dencim <43787288+dencim@users.noreply.github.com>
2020-10-05 14:21:35 -0400

    Add insert function

--- Max Heap.py
+++ Max Heap.py
@@ -64,8 +64,15 @@ class MaxHeap:
         
         return extracted
     
-    def insert(self):
-        pass
+    def insert(self, x):
+        self.heap.append(x)
+        i = len(self.heap)
+        while( self.heap[i-1] > self.heap[self.parent(i)-1]):
+            #swap
+            temp = self.heap[self.parent(i)-1]
+            self.heap[self.parent(i)-1] = self.heap[i-1]
+            self.heap[i-1] = temp
+            i = self.parent(i-1)
     
     def print_as_array(self):
         print(self.heap)
@@ -84,9 +91,12 @@ class MaxHeap:
             
 #h = MaxHeap([16,4,10,14,7,9,3,2,8,1])
 h2 = MaxHeap([4,1,3,2,16,9,10,14,8,7])
+h3 = MaxHeap([16,14,10,8,7,9,3,2,4,1])
 #h.print_as_tree()
 #h.max_heapify(1)
 #h.print_as_array()
+#h2.print_as_array()
+#print(h2.extract_max())
 h2.print_as_array()
-print(h2.extract_max())
-h2.print_as_array()
+h3.insert(15)
+h3.print_as_array()
